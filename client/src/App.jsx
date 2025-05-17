ffa2a7b08ebd5dca744d77a69deb4317ce25eac0
sankarshan <ssankarshan636@gmail.com>
2025-05-17 22:33:10 +0530

    Add Navbar component and integrate react-router-dom for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-    <div class='font-bold text-yellow-500 bg-black'>App</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
